31f713de682ddccd4acf5bd3d39147e6ee1831f8
douglas engstrand <doueng09@gmail.com>
2019-05-21 09:27:31 +0200

    level08

---
+++ level09/rofl.py
@@ -0,0 +1,11 @@
+import struct
+
+# i fun
+# secret_backdoor
+# handle_msg
+# set_msg
+# set_username
+# main
+
+# i var
+# save  0x0000000000202080
---
+++ level09/source.c
@@ -0,0 +1,55 @@
+void secret_backdoor()
+{
+    char b[0x80];
+
+    fgets(b, 0x80, stdin);
+    system(b);
+}
+
+void set_msg(char *arg)
+{
+    char b[0x80*4];
+
+    bzero(b, 0x80*4);
+    puts(">: Msg @Unix-Dude");
+    printf(">>: ");
+    // vararg // >: Msg @Unix-Dude\naaaaaaaaaaaaaaa\n----------\n
+    strncpy(arg, b, fgets(b, 0x400, stdin));
+
+}
+
+void set_username()
+{
+    int     i;
+    char    b[0x80];
+
+    puts(">: Enter your username");
+    printf(">>: ");
+    fgets(b, 0x80, stdin);
+    i = 0;
+    while (i < 0x28)
+    {
+        i++;
+    }
+    printf(">: Welcome, %s", %b);
+}
+
+void handle_msg()
+{
+    int     n;
+    char    name[0x28];
+    char    msg[0x8c];
+
+    bzero(b, 0x28);
+    n = 0x8c;
+    set_username(name);
+    set_msg(msg);
+    puts(">: Msg sent!");
+}
+
+int main()
+{
+    puts("--------------------------------------------\n|   ~Welcome to l33t-m$n ~    v1337        |\n--------------------------------------------");
+    handle_msg();
+    return 0;
+}
